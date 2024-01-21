e0606e673e0f323d674456a2a29fbd23a24722e8
Mohammad Afzal Malik <142798474+afzalmalik13@users.noreply.github.com>
2024-01-22 00:02:10 +0530

    Add files via upload

---
+++ Python_Notes/16_Methods_in_list/02_extend.py
@@ -0,0 +1,7 @@
+# extend() - Add the elements of a list (or any iterable), to the end of the current list
+
+lst1 = ['Dragon Ball', 'Naruto', 'One Piece']
+print(lst1)
+lst2 = ['Demon Slayer', 'Tokyo Revenger', 'One Punch Man']
+lst1.extend(lst2)
+print(lst1)
